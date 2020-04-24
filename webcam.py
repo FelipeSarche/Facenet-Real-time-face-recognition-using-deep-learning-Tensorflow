b1fd962867dcf8620b2ef8490a9a5c12eeff35a4
felipe sarche <sarche_2004@hotmail.com>
2020-04-23 22:30:28 -0500

    Capturas de pantalla con texto escrito

--- webcam.py
+++ webcam.py
@@ -1,13 +1,34 @@
 import cv2
+import os
+import time
+from datetime import datetime
+
 camera = cv2.VideoCapture(0)
+
+directorioCapturas ="../fotosPersonal"
+
+
+try:
+  os.stat(directorioCapturas)
+except:
+  os.mkdir(directorioCapturas)
+  
+
+
 while True:
     return_value,image = camera.read()
     gray = image
-    fuente = cv2.FONT_HERSHEY_SCRIPT_COMPLEX
-    cv2.putText(gray, text='HOLA', org=(20,100), fontFace=fuente, fontScale=1, color=(0,255,0), thickness=4, lineType=cv2.LINE_8)	
+    fuente = cv2.FONT_HERSHEY_COMPLEX_SMALL
+    	
     cv2.imshow('image',gray)
-    if cv2.waitKey(1)& 0xFF == ord('s'):
+    
+    pressed_key = cv2.waitKey(1)	
+    if pressed_key& 0xFF == ord('s'):
+	cv2.putText(gray, text='HOLA', org=(20,100), fontFace=fuente, fontScale=2, color=(0,255,0), thickness=1, lineType=cv2.LINE_8)
+	cv2.imshow('image',gray)
         cv2.imwrite('test.jpg',image)
-        break
+	cv2.waitKey(500)       
+    if pressed_key& 0xFF == ord('q'):
+	break
 camera.release()
 cv2.destroyAllWindows()
