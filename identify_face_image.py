879befd967b103913a596e7858e36f6d64c568b2
felipe sarche <sarche_2004@hotmail.com>
2020-04-27 15:03:03 -0500

    guardado por fechas en carpetas con fecha

--- identify_face_image.py
+++ identify_face_image.py
@@ -14,7 +14,7 @@ import sys
 from datetime import datetime
 
 
-img_path='abc.jpg'
+
 modeldir = './model/20170511-185253.pb'
 classifier_filename = './class/classifier.pkl'
 npy='./npy'
@@ -25,8 +25,14 @@ prediccionSetPoint = 0.5 #valor de predicc
 directorioImagenesClasificadas ="../Clasificada"
 directorioImagenesReconocidas ="../Clasificada/Reconocidas"
 directorioImagenesDesconocidas ="../Clasificada/Desconocidas"
-directorioImagenesSinClasificar = "/run/user/1000/gvfs/smb-share:server=raspberrypi.local,share=raw/2020-04-25"
+directorioImagenesSinClasificar = "/run/user/1000/gvfs/smb-share:server=raspberrypi.local,share=raw/2020-04-26"
 
+#listachar = directorioImagenesSinClasificar.split('/')
+#print("*******************************************    " + listachar[-1]  )
+listadirectorios = "/run/user/1000/gvfs/smb-share:server=raspberrypi.local,share=raw"
+listadecarpetas = os.listdir(listadirectorios)
+listadecarpetas.sort()
+print(listadecarpetas)
 
 
 try:
@@ -37,129 +43,147 @@ except:
   os.mkdir(directorioImagenesDesconocidas)
   os.mkdir(directorioImagenesReconocidas)
 
-ListaImagenes = os.listdir(directorioImagenesSinClasificar)
-ListaImagenes.sort()
 
-for imagensinclasificar in ListaImagenes:
-	print("Nombre de imagen: " + str(imagensinclasificar))
-	with tf.Graph().as_default():
-	    gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.6)
-	    sess = tf.Session(config=tf.ConfigProto(gpu_options=gpu_options, log_device_placement=False))
-	    with sess.as_default():
-		pnet, rnet, onet = detect_face.create_mtcnn(sess, npy)
+for carpetasacrear in listadecarpetas:
+	try:
+	  os.stat(directorioImagenesReconocidas+"/"+str(carpetasacrear))
 
-		minsize = 20  # minimum size of face
-		threshold = [0.6, 0.7, 0.7]  # three steps's threshold
-		factor = 0.709  # scale factor
-		margin = 44
-		frame_interval = 3
-		batch_size = 1000
-		image_size = 182
-		input_image_size = 160
-		
-		HumanNames = os.listdir(train_img)
-		HumanNames.sort()
+	except:
+	  os.mkdir(directorioImagenesReconocidas+"/"+str(carpetasacrear))
+	try:
+	  os.stat(directorioImagenesDesconocidas+"/"+str(carpetasacrear))
 
-		print('Loading feature extraction model')
-		facenet.load_model(modeldir)
+	except:
+	  os.mkdir(directorioImagenesDesconocidas+"/"+str(carpetasacrear))	 
 
-		images_placeholder = tf.get_default_graph().get_tensor_by_name("input:0")
-		embeddings = tf.get_default_graph().get_tensor_by_name("embeddings:0")
-		phase_train_placeholder = tf.get_default_graph().get_tensor_by_name("phase_train:0")
-		embedding_size = embeddings.get_shape()[1]
+	directorioactual = str(listadirectorios) + "/" + str(carpetasacrear)
+	ListaImagenes = os.listdir(directorioactual)
+	print("***********************************                "+directorioactual)
+	ListaImagenes.sort()
+
+	for imagensinclasificar in ListaImagenes:
+		print("Nombre de imagen: " + str(imagensinclasificar))
+		with tf.Graph().as_default():
+		    gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.6)
+		    sess = tf.Session(config=tf.ConfigProto(gpu_options=gpu_options, log_device_placement=False))
+		    with sess.as_default():
+			pnet, rnet, onet = detect_face.create_mtcnn(sess, npy)
+
+			minsize = 20  # minimum size of face
+			threshold = [0.6, 0.7, 0.7]  # three steps's threshold
+			factor = 0.709  # scale factor
+			margin = 44
+			frame_interval = 3
+			batch_size = 1000
+			image_size = 182
+			input_image_size = 160
+			
+			HumanNames = os.listdir(train_img)
+			HumanNames.sort()
+
+			print('Loading feature extraction model')
+			facenet.load_model(modeldir)
+
+			images_placeholder = tf.get_default_graph().get_tensor_by_name("input:0")
+			embeddings = tf.get_default_graph().get_tensor_by_name("embeddings:0")
+			phase_train_placeholder = tf.get_default_graph().get_tensor_by_name("phase_train:0")
+			embedding_size = embeddings.get_shape()[1]
 
 
-		classifier_filename_exp = os.path.expanduser(classifier_filename)
-		with open(classifier_filename_exp, 'rb') as infile:
-		    (model, class_names) = pickle.load(infile)
+			classifier_filename_exp = os.path.expanduser(classifier_filename)
+			with open(classifier_filename_exp, 'rb') as infile:
+			    (model, class_names) = pickle.load(infile)
 
-		# video_capture = cv2.VideoCapture("akshay_mov.mp4")
-		c = 0
+			# video_capture = cv2.VideoCapture("akshay_mov.mp4")
+			c = 0
 
 
-		print('Start Recognition!')
-		prevTime = 0
-		# ret, frame = video_capture.read()
+			print('Start Recognition!')
+			prevTime = 0
+			# ret, frame = video_capture.read()
 
-		frame = cv2.imread(directorioImagenesSinClasificar+ "/" + str(imagensinclasificar),0)
+			frame = cv2.imread(directorioactual + "/" + str(imagensinclasificar),0)
 
-		frame = cv2.resize(frame, (0,0), fx=0.5, fy=0.5)    #resize frame (optional)
+			frame = cv2.resize(frame, (0,0), fx=0.5, fy=0.5)    #resize frame (optional)
 
-		curTime = time.time()+1    # calc fps
-		timeF = frame_interval
+			curTime = time.time()+1    # calc fps
+			timeF = frame_interval
 
-		if (c % timeF == 0):
-		    find_results = []
+			if (c % timeF == 0):
+			    find_results = []
 
-		    if frame.ndim == 2:
-		        frame = facenet.to_rgb(frame)
-		    frame = frame[:, :, 0:3]
-		    bounding_boxes, _ = detect_face.detect_face(frame, minsize, pnet, rnet, onet, threshold, factor)
-		    nrof_faces = bounding_boxes.shape[0]
-		    print('Face Detected: %d' % nrof_faces)
+			    if frame.ndim == 2:
+				frame = facenet.to_rgb(frame)
+			    frame = frame[:, :, 0:3]
+			    bounding_boxes, _ = detect_face.detect_face(frame, minsize, pnet, rnet, onet, threshold, factor)
+			    nrof_faces = bounding_boxes.shape[0]
+			    print('Face Detected: %d' % nrof_faces)
 
-		    if nrof_faces > 0:
-		        det = bounding_boxes[:, 0:4]
-		        img_size = np.asarray(frame.shape)[0:2]
+			    if nrof_faces > 0:
+				det = bounding_boxes[:, 0:4]
+				img_size = np.asarray(frame.shape)[0:2]
 
-		        cropped = []
-		        scaled = []
-		        scaled_reshape = []
-		        bb = np.zeros((nrof_faces,4), dtype=np.int32)
+				cropped = []
+				scaled = []
+				scaled_reshape = []
+				bb = np.zeros((nrof_faces,4), dtype=np.int32)
 
-		        for i in range(nrof_faces):
-		            emb_array = np.zeros((1, embedding_size))
+				for i in range(nrof_faces):
+				    emb_array = np.zeros((1, embedding_size))
 
-		            bb[i][0] = det[i][0]
-		            bb[i][1] = det[i][1]
-		            bb[i][2] = det[i][2]
-		            bb[i][3] = det[i][3]
+				    bb[i][0] = det[i][0]
+				    bb[i][1] = det[i][1]
+				    bb[i][2] = det[i][2]
+				    bb[i][3] = det[i][3]
 
-		            # inner exception
-		            if bb[i][0] <= 0 or bb[i][1] <= 0 or bb[i][2] >= len(frame[0]) or bb[i][3] >= len(frame):
-		                print('face is too close')
-		                continue
+				    # inner exception
+				    if bb[i][0] <= 0 or bb[i][1] <= 0 or bb[i][2] >= len(frame[0]) or bb[i][3] >= len(frame):
+				        print('face is too close')
+				        continue
 
-		            cropped.append(frame[bb[i][1]:bb[i][3], bb[i][0]:bb[i][2], :])
-		            cropped[i] = facenet.flip(cropped[i], False)
-		            scaled.append(misc.imresize(cropped[i], (image_size, image_size), interp='bilinear'))
-		            scaled[i] = cv2.resize(scaled[i], (input_image_size,input_image_size),
-		                                   interpolation=cv2.INTER_CUBIC)
-		            scaled[i] = facenet.prewhiten(scaled[i])
-		            scaled_reshape.append(scaled[i].reshape(-1,input_image_size,input_image_size,3))
-		            feed_dict = {images_placeholder: scaled_reshape[i], phase_train_placeholder: False}
-		            emb_array[0, :] = sess.run(embeddings, feed_dict=feed_dict)
-		            predictions = model.predict_proba(emb_array)
-		            print(predictions)
-		            best_class_indices = np.argmax(predictions, axis=1)
-		            # print(best_class_indices)
-		            best_class_probabilities = predictions[np.arange(len(best_class_indices)), best_class_indices]
-		            print(best_class_probabilities)
-		            cv2.rectangle(frame, (bb[i][0], bb[i][1]), (bb[i][2], bb[i][3]), (0, 255, 0), 2)    #boxing face
+				    cropped.append(frame[bb[i][1]:bb[i][3], bb[i][0]:bb[i][2], :])
+				    cropped[i] = facenet.flip(cropped[i], False)
+				    scaled.append(misc.imresize(cropped[i], (image_size, image_size), interp='bilinear'))
+				    scaled[i] = cv2.resize(scaled[i], (input_image_size,input_image_size),
+				                           interpolation=cv2.INTER_CUBIC)
+				    scaled[i] = facenet.prewhiten(scaled[i])
+				    scaled_reshape.append(scaled[i].reshape(-1,input_image_size,input_image_size,3))
+				    feed_dict = {images_placeholder: scaled_reshape[i], phase_train_placeholder: False}
+				    emb_array[0, :] = sess.run(embeddings, feed_dict=feed_dict)
+				    predictions = model.predict_proba(emb_array)
+				    print(predictions)
+				    best_class_indices = np.argmax(predictions, axis=1)
+				    # print(best_class_indices)
+				    best_class_probabilities = predictions[np.arange(len(best_class_indices)), best_class_indices]
+				    print(best_class_probabilities)
+				    cv2.rectangle(frame, (bb[i][0], bb[i][1]), (bb[i][2], bb[i][3]), (0, 255, 0), 2)    #boxing face
 
-		            #plot result idx under box
-		            text_x = bb[i][0]
-		            text_y = bb[i][3] + 20
-		            print('Result Indices: ', best_class_indices[0])
-		            print(HumanNames)
-		            for H_i in HumanNames:
-		                # print(H_i)
-		                if HumanNames[best_class_indices[0]] == H_i:
-				    if best_class_probabilities>prediccionSetPoint:
-		                    	result_names = HumanNames[best_class_indices[0]]
-				    else:
-					result_names = "Desconocido"
-		                    cv2.putText(frame, result_names, (text_x, text_y), cv2.FONT_HERSHEY_COMPLEX_SMALL,
-		                                1, (0, 0, 255), thickness=1, lineType=2)
-		    else:
-		        print('Unable to align')
-		#cv2.imshow('Image', frame)
-		created= os.stat(directorioImagenesSinClasificar + "/" + imagensinclasificar).st_ctime
-		if best_class_probabilities>prediccionSetPoint:
-			cv2.imwrite(directorioImagenesReconocidas + "/"  + str(datetime.fromtimestamp(created)) + " " + str(result_names) + ".png",frame)
-		else:
-			cv2.imwrite(directorioImagenesDesconocidas + "/" + str(datetime.fromtimestamp(created)) + " " + str(result_names) + ".png",frame)
-		cv2.destroyAllWindows()
+				    #plot result idx under box
+				    text_x = bb[i][0]
+				    text_y = bb[i][3] + 20
+				    print('Result Indices: ', best_class_indices[0])
+				    print(HumanNames)
+				    for H_i in HumanNames:
+				        # print(H_i)
+				        if HumanNames[best_class_indices[0]] == H_i:
+					    if best_class_probabilities>prediccionSetPoint:
+				            	result_names = HumanNames[best_class_indices[0]]
+					    else:
+						result_names = "Desconocido"
+				            cv2.putText(frame, result_names, (text_x, text_y), cv2.FONT_HERSHEY_COMPLEX_SMALL,
+				                        1, (0, 0, 255), thickness=1, lineType=2)
+			    else:
+				print('Unable to align')
+			#cv2.imshow('Image', frame)
+			listachar = directorioactual.split('/')
+			created= os.stat(directorioactual + "/" + imagensinclasificar).st_ctime
+			if best_class_probabilities>prediccionSetPoint:
+				#cv2.imwrite(directorioImagenesReconocidas + "/"   + " " + str(result_names) + ".png",frame)
+				cv2.imwrite(directorioImagenesReconocidas + "/"  + listachar[-1] + "/" +str(datetime.fromtimestamp(created)) + " " + str(result_names) + ".png",frame)
+			else:
+				cv2.imwrite(directorioImagenesDesconocidas + "/" + listachar[-1] + "/" +str(datetime.fromtimestamp(created)) + " " + str(result_names) + ".png",frame)
+				#cv2.imwrite(directorioImagenesDesconocidas + "/" + " " + str(result_names) + ".png",frame)
+			cv2.destroyAllWindows()
 
 
 if cv2.waitKey(1000000) & 0xFF == ord('q'):
